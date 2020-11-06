d5f05da11146d991facb722c84154595d8e001fd
Brandon Norris <blnorris@gmail.com>
2020-11-05 18:35:58 -0600

    updates

---
+++ Exploder.lua
@@ -0,0 +1,80 @@
+--Z going positive is south
+--X going positive is east
+local startX = -916
+local startZ = -896
+local endX = -885
+local endZ = -939
+local refuelSlot = 16
+local witherSlot = 15
+
+local x, y, z = gps.locate(5)
+
+
+
+function Explode()
+    turtle.select(witherSlot)
+    turtle.placeUp()
+
+    turtle.back()
+    turtle.up()
+
+    local counter = 0
+    while counter < 100 do
+        redstone.setOutput("front", not redstone.getOutput("front"))
+        sleep(0.2)
+        counter = counter + 1
+    end
+
+end
+
+function Refuel()
+    turtle.select(refuelSlot)
+    turtle.refuel(1)
+end
+function MoveForward()
+    turtle.select(up)
+    while turtle.inspect()
+    do 
+        turtle.dig()
+    end
+    turtle.digUp()
+    turtle.forward()
+end
+function GetPosition()
+    x, y, z = gps.locate(5)
+end
+
+
+while endX >x do
+    
+    if turtle.getFuelLevel() == 0 
+    then Refuel()
+    end
+
+    if endZ > z
+    then
+        print(endZ)
+        print(z)
+        turtle.turnRight()
+        MoveForward()
+        MoveForward()
+        turtle.turnRight()
+        MoveForward()
+    end
+
+    if startZ < z
+    then
+        turtle.turnLeft()
+        MoveForward()
+        MoveForward()
+        turtle.turnLeft()
+        MoveForward()
+    end
+
+
+    MoveForward()
+    MoveForward()
+
+    
+    x, y, z = gps.locate(5)
+end
